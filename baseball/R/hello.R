00738a4b5318bc527afe63ea5efa03bb1060923b
Adam Funk <funkaj@vcu.edu>
2021-10-27 00:37:29 -0400

    A pair of functions to be used in the package, namely a summary function and a summry function.

--- baseball/R/hello.R
+++ baseball/R/hello.R
@@ -1,5 +1,5 @@
 #' A function to say hello
-#' @return A statment saying hello to comething
+#' @return A statement saying hello to something
 #' @export
 
 
@@ -15,4 +15,30 @@ hello <- function() {
 
 huh <- function() {
   print("What did you say?")
+}
+
+#' Summary of Hitting
+#' @param x The dataset to be summarize, by default hitting.
+#' @return A summary of the dataset hitting
+#' @export
+
+hit_summary <- function(x = hitting) {
+  summary(x)
+}
+
+#'Team Average Plot
+#'@param x The dataset to be summarize, by default hitting.
+#'@param tm The team to be looked for their averages over time
+#'@return A plot of the averages of all teams, with a smoothing curve, and the team of interest highlighted in red points.
+#'@export
+#'
+
+team_avg <- function(x = hitting, tm = "MIN"){
+  avg <- x %>% group_by(teamID, yearID) %>% summarise(AVERAGE = sum(H)/sum(AB))
+  team <- x %>% filter(teamID == "CIN") %>% group_by(yearID) %>% summarise(AVERAGE = sum(H)/sum(AB))
+  ggplot(avg, aes(x = yearID, y = AVERAGE)) +
+    geom_point(alpha = 1/15) +
+    geom_point(data = team, aes(x = yearID, y = AVERAGE, col = "red"), pch = 19, cex = 3)+
+    geom_smooth() +
+    theme(legend.position = "none")
 }
--- baseball/data-raw/hitting.R
+++ baseball/data-raw/hitting.R
@@ -1,3 +1,5 @@
 ## code to prepare `hitting` dataset goes here
 
+hitting <- read.csv("C:/Users/afunk/Desktop/git/baseball/Batting.csv")
+
 usethis::use_data(hitting, overwrite = TRUE)
