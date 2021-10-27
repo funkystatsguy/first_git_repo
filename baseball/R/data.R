faec7dee9085b9ccacdc9a257fe25e4e5c8db3b7
Adam Funk <funkaj@vcu.edu>
2021-10-26 22:01:17 -0400

    Adding the dataset "hitting" with its description to the package

---
+++ baseball/R/data.R
@@ -0,0 +1,29 @@
+#' @title Batting.csv
+#' @description A comprehensive list of all batters from 1871 to 2020 citing almost all results of an at bat.
+#' @format A data frame with 108789 rows and 22 variables:
+#' \describe{
+#'   \item{\code{playerID}}{character The identifier for the player}
+#'   \item{\code{yearID}}{integer The year of the season}
+#'   \item{\code{stint}}{integer For players with multiple teams in a season, this lists which team it was for that player in the year}
+#'   \item{\code{teamID}}{character The team ID the player played for}
+#'   \item{\code{lgID}}{character The league the team is in at the time}
+#'   \item{\code{G}}{integer Number of games the player played in}
+#'   \item{\code{AB}}{integer Number of at bats the player had for the team that season}
+#'   \item{\code{R}}{integer Number of runs the player scored for the team that season}
+#'   \item{\code{H}}{integer Number of hits the player had for the team that season}
+#'   \item{\code{X2B}}{integer Number of doubles the player had for the team that season}
+#'   \item{\code{X3B}}{integer Number of triples the player had for the team that season}
+#'   \item{\code{HR}}{integer Number of home runs the player had for the team that season}
+#'   \item{\code{RBI}}{integer Number of runs batted in the player had for the team that season}
+#'   \item{\code{SB}}{integer Number of stolen bases the player had for the team that season}
+#'   \item{\code{CS}}{integer Number of times the player was caught stealing during the time with the team that season}
+#'   \item{\code{BB}}{integer Number of walks the player had for the team that season}
+#'   \item{\code{SO}}{integer NUmber of strikeouts the player had for the team that season}
+#'   \item{\code{IBB}}{integer Number of intentional walks the player had for the team that season}
+#'   \item{\code{HBP}}{integer Number of times the player was hit by a pitch for the team that season}
+#'   \item{\code{SH}}{integer Number of sacrifice hits the player had for the team that season}
+#'   \item{\code{SF}}{integer Number of sacrifice flies the player had for the team that season}
+#'   \item{\code{GIDP}}{integer Number of times the player grounded out into a double play for the team that season}
+#'}
+#' @details DETAILS
+"hitting"
---
+++ baseball/data-raw/hitting.R
@@ -0,0 +1,3 @@
+## code to prepare `hitting` dataset goes here
+
+usethis::use_data(hitting, overwrite = TRUE)
